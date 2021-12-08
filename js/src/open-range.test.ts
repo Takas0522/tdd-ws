3a1a9424ea4932ee63513266f1a4b114f6783986
Takas0522 <t_o_19880522@outlook.com>
2021-12-08 16:26:21 +0900

    test fix

--- js/src/open-range.test.ts
+++ js/src/open-range.test.ts
@@ -11,15 +11,15 @@ describe('Closed Range', () => {
       });
     });
     describe ('整数開区間インスタンスは文字列表現を持つ', () => {
-      it ('整数開区間インスタンス生成時に3と8を指定すると、インスタンスのtoString()は[3,8]を返却する', () => {
+      it ('整数開区間インスタンス生成時に3と8を指定すると、インスタンスのtoString()は(3,8)を返却する', () => {
         const target = new OpenRange(3,8);
         const asc = target.toString();
-        expect(asc).toEqual('[3,8]');
+        expect(asc).toEqual('(3,8)');
       });
-      it ('整数開区間インスタンス生成時に同値(1,1)を指定すると、インスタンスのtoString()は[1,1]を返却する', () => {
+      it ('整数開区間インスタンス生成時に同値(1,1)を指定すると、インスタンスのtoString()は(1,1)を返却する', () => {
         const target = new OpenRange(1,1);
         const asc = target.toString();
-        expect(asc).toEqual('[1,1]');
+        expect(asc).toEqual('(1,1)');
       });
     });
   });
@@ -36,7 +36,7 @@ describe('Closed Range', () => {
         test.each([
           { lower: 1, upper: 4, containCheckValue: 2 },
           { lower: 1, upper: 4, containCheckValue: 3 }
-        ])('[$lower,$upper]整数開区間で、isContainに$containCheckValueが指定された場合trueが返却される', ({lower, upper, containCheckValue}) => {
+        ])('($lower,$upper)整数開区間で、isContainに$containCheckValueが指定された場合trueが返却される', ({lower, upper, containCheckValue}) => {
           const target = new OpenRange(lower, upper);
           expect(target.isContain(containCheckValue)).toBeTruthy();
         });
@@ -45,7 +45,7 @@ describe('Closed Range', () => {
         test.each([
           { lower: 1, upper: 4, containCheckValue: 1 },
           { lower: 1, upper: 4, containCheckValue: 4 }
-        ])('[$lower,$upper]整数開区間で、isContainに$containCheckValueが指定された場合falseが返却される', ({lower, upper, containCheckValue}) => {
+        ])('($lower,$upper)整数開区間で、isContainに$containCheckValueが指定された場合falseが返却される', ({lower, upper, containCheckValue}) => {
           const target = new OpenRange(lower, upper);
           expect(target.isContain(containCheckValue)).toBeFalsy();
         });
@@ -53,7 +53,7 @@ describe('Closed Range', () => {
     });
     describe ('別の整数開区間インスタンスと等価かどうかが判定できる', () => {
       describe('等価である整数開区間インスタンスが指定された場合trueが返却される', () => {
-        it (' [1,4]整数開区間インスタンスのisEqualに別の[1,4]整数開区間インスタンスを指定した場合trueが返却される', () => {
+        it ('(1,4)整数開区間インスタンスのisEqualに別の(1,4)整数開区間インスタンスを指定した場合trueが返却される', () => {
           const base = new OpenRange(1, 4);
           const comparison = new OpenRange(1, 4);
           expect(base.isEqual(comparison)).toBeTruthy();
@@ -65,7 +65,7 @@ describe('Closed Range', () => {
           { basetLower: 1, baseUpper: 4, comparisonLower: 1, comparisonUpper: 5 },
           { basetLower: 1, baseUpper: 4, comparisonLower: 0, comparisonUpper: 4 },
           { basetLower: 1, baseUpper: 4, comparisonLower: 1, comparisonUpper: 3 },
-        ])('[$basetLower, $baseUpper]整数開区間インスタンスの、isEqualに別の[$comparisonLower,$comparisonUpper]整数開区間インスタンスが指定された場合falseが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
+        ])('($basetLower,$baseUpper)整数開区間インスタンスの、isEqualに別の($comparisonLower,$comparisonUpper)整数開区間インスタンスが指定された場合falseが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
           const base = new OpenRange(basetLower, baseUpper);
           const comparison = new OpenRange(comparisonLower, comparisonUpper);
           expect(base.isEqual(comparison)).toBeFalsy();
@@ -76,7 +76,7 @@ describe('Closed Range', () => {
       describe('完全に含まれる整数開区間インスタンスが指定された場合trueが返却される', () => {
         test.each([
           { basetLower: 1, baseUpper: 4, comparisonLower: 2, comparisonUpper: 3 },
-        ])('[$basetLower,$baseUpper]整数開区間インスタンスのisIncludeに[$comparisonLower,$comparisonUpper]整数開区間インスタンスを指定した場合trueが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
+        ])('($basetLower,$baseUpper)整数開区間インスタンスのisIncludeに($comparisonLower,$comparisonUpper)整数開区間インスタンスを指定した場合trueが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
           const base = new OpenRange(basetLower, baseUpper);
           const comparison = new OpenRange(comparisonLower, comparisonUpper);
           expect(base.isFullInclude(comparison)).toBeTruthy();
@@ -87,7 +87,7 @@ describe('Closed Range', () => {
           { basetLower: 1, baseUpper: 4, comparisonLower: 1, comparisonUpper: 3 },
           { basetLower: 1, baseUpper: 4, comparisonLower: 2, comparisonUpper: 4 },
           { basetLower: 1, baseUpper: 4, comparisonLower: 1, comparisonUpper: 4 },
-        ])('[$basetLower,$baseUpper]整数開区間インスタンスのisIncludeに[$comparisonLower,$comparisonUpper]整数開区間インスタンスを指定した場合falseが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
+        ])('($basetLower,$baseUpper)整数開区間インスタンスのisIncludeに($comparisonLower,$comparisonUpper)整数開区間インスタンスを指定した場合falseが返却される', ({basetLower, baseUpper, comparisonLower, comparisonUpper}) => {
           const base = new OpenRange(basetLower, baseUpper);
           const comparison = new OpenRange(comparisonLower, comparisonUpper);
           expect(base.isFullInclude(comparison)).toBeFalsy();
